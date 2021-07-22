a693f6ee8e6794dce1c87ddc355f8761cbdd60a6
Timo Lindfors <timo.lindfors@iki.fi>
2021-07-22 12:20:55 +0300

    Add support for specifying machine to use

--- client/baremetal_queue_client.py
+++ client/baremetal_queue_client.py
@@ -5,9 +5,11 @@ import sys
 import time
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Submit image for testing with baremetal_http_server.py")
+    parser = argparse.ArgumentParser(description="Submit image for testing with baremetal_queue_server.py")
     parser.add_argument("-u", "--url", metavar="URL", required="True", help="Service URL")
     parser.add_argument("-o", "--output", metavar="TARFILE", required=True, help="Write output to FILE")
+    parser.add_argument("--chunk-size", default=10, help="Chunk size in MB")
+    parser.add_argument("--machine", required=True, help="Machine to use")
     parser.add_argument("--api-key", metavar="FILE", required=True, help="File with API key")
     parser.add_argument("image", metavar="FILE", help="Disk image to run")
     args = parser.parse_args()
@@ -19,17 +21,22 @@ if __name__ == "__main__":
     headers = {
         "X-API-KEY": apikey
     }
-    r = requests.post(args.url, headers=headers)
-    assert r.status_code == 200
+    data = {
+        "machine": args.machine
+    }
+    r = requests.post(args.url, headers=headers, json=data)
+    if r.status_code != 200:
+        print("Server replide with status code {code} and error: {error}".format(code=r.status_code, error=r.text))
+        sys.exit(1)
     job_id = r.json()["job_id"]
     print("Job {} was created".format(job_id))
 
-    print("Uploading image in 10MB chunks")
+    print("Uploading image in {}MB chunks".format(args.chunk_size))
     chunk = 0
     with open(args.image, "rb") as imagefile:
         while True:
             print("chunk {}".format(chunk))
-            data = imagefile.read(10*1024*1024)
+            data = imagefile.read(args.chunk_size*1024*1024)
             if data == b"":
                 break
             url = "{}/{}/upload-chunk".format(args.url, job_id)
