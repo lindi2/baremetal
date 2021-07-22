acc39cdcbc54c72866c992f53de864011f2f70d5
Timo Lindfors <timo.lindfors@iki.fi>
2021-07-22 12:19:16 +0300

    Allow specifying port

--- runner/serial_logger.py
+++ runner/serial_logger.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 import socket
 import sys
+import argparse
 
-outputfilename = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", default="localhost", metavar="ADDR", help="Connect to host HOST")
+parser.add_argument("--port", required=True, type=int, metavar="PORT", help="Connect to port PORT")
+parser.add_argument("--output", required=True, help="Output filename")
+args = parser.parse_args()
 
-with open(outputfilename, "wb") as output:
+with open(args.output, "wb") as output:
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("10.44.12.1", 2100))
+        s.connect((args.host, args.port))
         while True:
             data = s.recv(1024)
             if data == b"":
