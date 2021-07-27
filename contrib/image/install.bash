89f2147f78bdcba98789db9998aacdc04885833a
Timo Lindfors <timo.lindfors@iki.fi>
2021-07-27 17:51:05 +0300

    Add tools for building template images

---
+++ contrib/image/install.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+set -x
+
+t="$(mktemp -d)"
+tar -czf "$t/late_command.tar.gz" late_command
+for variant in stable unstable; do
+    for bootmethod in bios uefi; do
+        name="baremetal_template_debian_${variant}_${bootmethod}"
+        if [ "$bootmethod" = "uefi" ]; then
+            bootargs=" --boot uefi"
+        else
+            bootargs=""
+        fi
+        rm -f "$name.img"
+        virt-install \
+            --connect "qemu:///session" \
+            --name "$name" \
+            --ram "2048" \
+            --disk "size=4,path=$name.img,format=raw,bus=scsi,discard=unmap,cache=unsafe" \
+            --controller "type=scsi,model=virtio-scsi" \
+            --initrd-inject "preseed.cfg" \
+            --initrd-inject "$t/late_command.tar.gz" \
+            --location "http://deb.debian.org/debian/dists/buster/main/installer-amd64/" \
+            --os-variant "debian10" \
+            --virt-type "kvm" \
+            --network "user,model=virtio" \
+            --controller "usb,model=none" \
+            --graphics "none" \
+            --transient \
+            $bootargs \
+            --extra-args "auto=true hostname=debian domain= console=ttyS0,115200n8 serial net.ifnames=0 variant=$variant"
+        virt-sparsify $name.img $name.sparse.img
+        lzop < $name.sparse.img > $name.img.lzo
+        rm -f $name.sparse.img
+    done
+done
+rm "$t/late_command.tar.gz"
+rmdir "$t"
---
+++ contrib/image/late_command/late_command.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -x
+set -e
+
+echo "Make sure the installation is as identical to manual install as possible"
+DEBIAN_FRONTEND=noninteractive apt-get install -y debconf-utils
+echo "keyboard-configuration keyboard-configuration/layoutcode string fi" | debconf-set-selections
+echo "keyboard-configuration keyboard-configuration/variant select Finnish" | debconf-set-selections
+echo "keyboard-configuration keyboard-configuration/xkb-keymap select fi" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive apt-get install -y console-setup console-setup-linux eject kbd keyboard-configuration usb.ids usbutils xkb-data
+apt-mark auto usb.ids console-setup-linux xkb-data kbd 
+if ! grep -q LANGUAGE /etc/default/locale; then
+    echo "$0: locale: Fixing LANGUAGE"
+    update-locale 'LANGUAGE="en_US:en"'
+fi
+apt-get remove -y debconf-utils
+apt-get clean
+
+if grep -q "variant=unstable" /proc/cmdline; then
+    echo "Upgrade to Debian unstable"
+    echo "deb http://deb.debian.org/debian/ sid main" > /etc/apt/sources.list
+    apt-get update
+    apt-get -y dist-upgrade
+fi
+
+echo "Reconfigure grub2"
+sed -i "s@GRUB_CMDLINE_LINUX_DEFAULT=.*@GRUB_CMDLINE_LINUX_DEFAULT=\"quiet net.ifnames=0\"@" /etc/default/grub
+update-grub2
+
+echo "Install reporting service"
+cp baremetal-report /usr/local/bin/
+cp baremetal-report.service /etc/systemd/system/
+systemctl enable baremetal-report.service
+
+echo "Minimize image size"
+apt-get -y install apt-show-versions
+apt-get -y purge bind9-host discover-data openssh-client python2 python2.7 python2.7-minimal
+rm -fr /lib/modules/*/kernel/{sound,drivers/media,drivers/isdn}
+apt-get autoremove -y
+set +x
+for i in $(find /lib/modules -name "*.ko"); do
+    if [ "$(file "$i"|grep ELF)" != "" ]; then
+        xz -v -1 $i
+        mv $i.xz $i
+    fi
+done
+set -x
+update-initramfs -k all -u
+apt-get clean
+fstrim -a
