f3f49cc776c426ab8830cdfb72dab1f23fb39844
Timo Lindfors <timo.lindfors@iki.fi>
2021-08-25 14:31:18 +0300

    Use more descriptive filenames, add certificate in PEM format

--- contrib/setup/secureboot/regenerate.bash
+++ contrib/setup/secureboot/regenerate.bash
@@ -3,6 +3,8 @@
 openssl req -config ./openssl.cnf \
         -new -x509 -newkey rsa:2048 \
         -nodes -days 36500 -outform DER \
-        -keyout "MOK.priv" \
-        -out "MOK.der"
+        -keyout "MOK.key.pem" \
+        -out "MOK.cert.der"
+
+openssl x509 -in MOK.cert.der -inform der -outform pem -out MOK.cert.pem
 
